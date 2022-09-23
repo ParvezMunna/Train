14a66934140b11a47a14dc1ccbf370e978fcea20
USER <USER@TeamOs-PC>
2022-09-23 12:33:02 +0600

    first

---
+++ src/Trihjk/hgb.java
@@ -0,0 +1,15 @@
+package Trihjk;
+
+import java.util.Scanner;
+
+public class hgb {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in)) {
+	String name=sc.next();
+	System.out.println(name);
+}
+	}
+
+}
